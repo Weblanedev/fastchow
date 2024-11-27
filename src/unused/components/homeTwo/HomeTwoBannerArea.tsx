564da364b323ade8bfc177daa30937317b0ac594
Bunmi Oye <bunmioye09@gmail.com>
2024-11-27 14:33:25 +0100

    update hero

--- src/unused/components/homeTwo/HomeTwoBannerArea.tsx
+++ src/unused/components/homeTwo/HomeTwoBannerArea.tsx
@@ -1,14 +1,14 @@
-"use client";
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination, EffectFade } from "swiper";
-import "swiper/css/bundle";
-import banner4 from "../../../public/assets/img/slider-img/banner-04.png";
-import banner5 from "../../../public/assets/img/slider-img/banner-05.png";
-import banner6 from "../../../public/assets/img/slider-img/banner-06.png";
-import bannerBg from "../../../public/assets/img/slider-img/banner-bg-1.png";
-import Link from "next/link";
-import Image from "next/image";
+'use client';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper';
+import 'swiper/css/bundle';
+import banner4 from '../../../../public/assets/img/slider-img/fastchow_burger.jpg';
+import banner5 from '../../../../public/assets/img/slider-img/fastchow-wings.jpg';
+import banner6 from '../../../../public/assets/img/slider-img/fastchow_pasta.jpg';
+import bannerBg from '../../../../public/assets/img/slider-img/dispatch.jpeg';
+import Link from 'next/link';
+import Image from 'next/image';
 const HomeTwoBannerArea = () => {
   const [activeIndex, setActiveIndex] = useState(0); // State to track active slide index
 
@@ -20,20 +20,20 @@ const HomeTwoBannerArea = () => {
     {
       id: 1,
       bgImg: banner4,
-      tagOne: "New Arrival",
-      tagTwo: "Hot Fashion Collection",
+      tagOne: 'Fast Delivery',
+      tagTwo: 'Hot Food Delivered To You',
     },
     {
       id: 2,
       bgImg: banner5,
-      tagOne: "On Trending",
-      tagTwo: "Autumn Fashion For Man",
+      tagOne: 'On Trending',
+      tagTwo: 'Food For All Seasons',
     },
     {
       id: 3,
       bgImg: banner6,
-      tagOne: "Featured Product",
-      tagTwo: "Perfect Fashion Unique Dress",
+      tagOne: 'Featured Service',
+      tagTwo: 'Perfect Food Delivery. Unique Experience',
     },
   ];
   return (
@@ -41,23 +41,26 @@ const HomeTwoBannerArea = () => {
       <div className="banner-area banner-area-2 pos-rel">
         <div
           className="banner-bg-thumb"
-          style={{ backgroundImage: `url(${bannerBg.src})` }}
+          style={{
+            backgroundImage: `url(${bannerBg.src})`,
+            backgroundSize: 'cover',
+          }}
         ></div>
-     
+
         <Swiper
           modules={[Autoplay, Navigation, Pagination, EffectFade]}
-          effect={"fade"}
+          effect={'fade'}
           onSlideChange={handleSlideChange}
           navigation={{
-            nextEl: ".slider-button-prev",
-            prevEl: ".slider-button-next",
+            nextEl: '.slider-button-prev',
+            prevEl: '.slider-button-next',
           }}
           pagination={{
-            el: ".slider2-pagination",
+            el: '.slider2-pagination',
             clickable: true,
           }}
           autoplay={{
-            delay: 3000,
+            delay: 5000,
           }}
           loop={true}
         >
@@ -69,16 +72,13 @@ const HomeTwoBannerArea = () => {
                   <div className="container">
                     <div className="row align-items-center">
                       <div className="col-xxl-7 col-xl-7 col-lg-7">
-                        <div className="banner-content pos-rel mb-30">
-                          <span className="banner-subtitle">{item?.tagOne}</span>
-                          <h1 className="banner-title">
-                            {item?.tagTwo}
-                          </h1>
+                        <div className="banner-content pos-rel mb-30 text-white">
+                          <span className="banner-subtitle">
+                            {item?.tagOne}
+                          </span>
+                          <h1 className="banner-title">{item?.tagTwo}</h1>
                           <div className="banner-btn">
-                            <Link
-                              className="fill-btn"
-                              href="/shop-sidebar-5-column"
-                            >
+                            <Link className="fill-btn" href="/contact">
                               Shop Now
                             </Link>
                           </div>
@@ -100,19 +100,18 @@ const HomeTwoBannerArea = () => {
         </Swiper>
 
         <div className="slider-nav d-none">
-            <div className="slider-button-prev">
-              <i className="fal fa-long-arrow-left"></i>
-            </div>
-            <div className="slider-button-next">
-              <i className="fal fa-long-arrow-right"></i>
-            </div>
+          <div className="slider-button-prev">
+            <i className="fal fa-long-arrow-left"></i>
           </div>
-          <div className="slider2-pagination-container">
-            <div className="container">
-              <div className="slider-pagination slider2-pagination"></div>
-            </div>
+          <div className="slider-button-next">
+            <i className="fal fa-long-arrow-right"></i>
           </div>
-       
+        </div>
+        <div className="slider2-pagination-container">
+          <div className="container">
+            <div className="slider-pagination slider2-pagination"></div>
+          </div>
+        </div>
       </div>
     </>
   );
